37c673333e49df58f45605c302b7a60ce32ba607
kaiyichen <chenkaiyi01@gmail.com>
2022-06-24 21:06:09 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# tailwindcss-58reda
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/tailwindcss-58reda)
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  content: [style.css, index.html, tailwind.css],
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+};
